2695fa802dfc079aaa3b7eca0b63ebbdfdc9c33f
Antonin Messinger <antonin.messinger@gmail.com>
2020-04-14 12:28:40 +0200

    refactor card sorting

--- src/Hand.js
+++ src/Hand.js
@@ -1,32 +1,17 @@
 import React from 'react'
 import Card from './Card'
-// import { groupBy } from 'lodash'
 import './CardList.css'
 
 export default function Hand({ cards }) {
-  const cardsGroups = ['FOOL', 'TRUMP', 'CLUB', 'DIAMOND', 'HEART', 'SPADE'].reduce((acc, type) => {
+  const cardsGroups = Object.values(['FOOL', 'TRUMP', 'CLUB', 'DIAMOND', 'HEART', 'SPADE'].reduce((acc, type) => {
     acc[type] = cards.filter(card => card.id.includes(type))
     return acc
-  }, {})
+  }, {})).flat()
+
   return (
     <div>
-      <div className="card-list">
-        {cardsGroups.FOOL.map((card, index) => <label key={index} className="selectable-card-container"><input type="radio" name="card" value={card.id} /><Card card={card} /></label>)}
-      </div>
-      <div className="card-list">
-        {cardsGroups.TRUMP.map((card, index) => <label key={index} className="selectable-card-container"><input type="radio" name="card" value={card.id} /><Card card={card} /></label>)}
-      </div>
-      <div className="card-list">
-        {cardsGroups.CLUB.map((card, index) => <label key={index} className="selectable-card-container"><input type="radio" name="card" value={card.id} /><Card card={card} /></label>)}
-      </div>
-      <div className="card-list">
-        {cardsGroups.DIAMOND.map((card, index) => <label key={index} className="selectable-card-container"><input type="radio" name="card" value={card.id} /><Card card={card} /></label>)}
-      </div>
-      <div className="card-list">
-        {cardsGroups.HEART.map((card, index) => <label key={index} className="selectable-card-container"><input type="radio" name="card" value={card.id} /><Card card={card} /></label>)}
-      </div>
-      <div className="card-list">
-        {cardsGroups.SPADE.map((card, index) => <label key={index} className="selectable-card-container"><input type="radio" name="card" value={card.id} /><Card card={card} /></label>)}
+      <div className="card-list hand" style={{ '--cards-count': cards.length }}>
+        {cardsGroups.map((card, index) => <label key={index} className="selectable-card-container"><input type="radio" name="card" value={card.id} /><Card card={card} /></label>)}
       </div>
     </div>
   )
